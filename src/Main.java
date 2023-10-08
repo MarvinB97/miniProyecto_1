7fad35baf5ca09667379a1ae951608513b4d03f4
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2023-10-08 16:18:54 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /*
  * ------------------------------------------------------------
  * Name Proyecto: miniProyecto-1
- * nombre participantes: Alejandro Garcia, Marvin Nuñez
+ * nombre participantes: Alejandro , Marvin Nuñez
  * File: Main.java
  * Relaciones: clase principal en donde se ejecuta el codigo
  * Ultima modificacion 06-10-23
@@ -11,3 +11,22 @@
 
 //Se importan las librerias
 import javax.swing.*;
+import modelo.*;
+
+
+public class Main {
+	
+  public static void main(String args[]) {
+	  
+	  JFrame frame = new JFrame("JUEGO DE FIGURAS");
+	  frame.setSize(600, 400);
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  
+	  Interfaz Juego = new Interfaz();
+	  frame.add(Juego);  
+    
+	  
+	  frame.setVisible(true);
+	  frame.setLocationRelativeTo(null); // la ubicamos en el centro de la pantalla
+  }
+}
