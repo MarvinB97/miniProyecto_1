6c08ec798d3639b9beef61f99191a1510166c90b
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2023-10-07 20:23:02 -0500

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * ------------------------------------------------------------
+ * Name Proyecto: miniProyecto-1
+ * nombre participantes: Alejandro Garcia, Marvin Nuñez
+ * File: Main.java
+ * Relaciones: clase principal en donde se ejecuta el codigo
+ * Ultima modificacion 06-10-23
+ * ------------------------------------------------------------
+ */
+
+
+//Se importan las librerias
+import javax.swing.*;
