8cec6d9c3969e73e4685332605216c64d25927c7
DocPaloma <weaqla.xd@gmail.com>
2023-10-08 09:38:24 -0500

    Update Sonido.java

--- src/modelo/Sonido.java
+++ src/modelo/Sonido.java
@@ -2,3 +2,23 @@ package modelo;
 
 import javax.sound.sampled.*;
 import java.io.*;
+
+
+public class Sonido {
+
+	public Sonido() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void realizarSonido(String nombreSonido){
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+	        System.out.println("Error al reproducir el sonido.");
+	        }
+	     }
+
+}
