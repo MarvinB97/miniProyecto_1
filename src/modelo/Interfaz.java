5c3116506c2acb443bd5a53c64817d9ecf93bfc0
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2023-10-08 20:37:09 -0500

    Update Interfaz.java

--- src/modelo/Interfaz.java
+++ src/modelo/Interfaz.java
@@ -1,10 +1,10 @@
 /*
  * ------------------------------------------------------------
  * Name Proyecto: miniProyecto-1
- * nombre participantes: Alejandro , Marvin Nuñez
+ * nombre participantes: Alejandro Medina Garcia, Marvin Brayan Nuñez Zuñiga
  * File: Interfaz.java
  * Relaciones: contiene los elementos de la GUI mas la logica del programa
- * Ultima Modificacion : 06-10-23
+ * Ultima Modificacion : 08-10-23
  * Pogramación Orientada a Objetos Grupo 80
  * ------------------------------------------------------------
  */
