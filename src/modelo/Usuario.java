9706ca31fa70e25928bb51dd3cf25d894431c14b
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2023-10-08 20:35:11 -0500

    Update Usuario.java

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,3 +1,15 @@
+/*
+ * ------------------------------------------------------------
+ * Name Proyecto: miniProyecto-1
+ * nombre participantes: Alejandro Medina Garcia, Marvin Nuñez
+ * File: Usuario.java
+ * Relaciones: Registro del nombre del usuario y de la puntuacion obtenida en el juego
+ * Ultima Modificacion : 06-10-23
+ * Pogramación Orientada a Objetos Grupo 80
+ * ------------------------------------------------------------
+ */
+
+
 package modelo;
 
 import java.util.*;
@@ -5,15 +17,18 @@ import java.util.stream.Collectors;
 
 
 public class Usuario {
-	
+
+	//Atributos
 	private List<String> nombresUsuarios;
 	private List<Integer> puntaje;
 
+	//CONSTRUCTOR
 	public Usuario() {
 		this.nombresUsuarios = new ArrayList<String>();
 		this.puntaje = new ArrayList<Integer>();
 	}
-	
+
+	//Metodos Getters
 	public int getIndice(String nombre) {
 		return this.nombresUsuarios.indexOf(nombre);
 	}
@@ -44,12 +59,14 @@ public class Usuario {
 		return this.nombresUsuarios.get(indice);
 	}
 	
-	
+
+	//Registro de nuevo usuario
 	public void agregarUsuario(String nombre) {
 		this.nombresUsuarios.add(nombre);
 		this.puntaje.add(0);
 	}
-	
+
+	//Cambio de la puntuacion
 	public void subirPuntaje(int indice) {
 		this.puntaje.set(indice, this.puntaje.get(indice)+1);
 	}
