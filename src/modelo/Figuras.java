9239a37c88d353741d48a651b36d4f7433146809
MarvinB97 <103372738+MarvinB97@users.noreply.github.com>
2023-10-08 20:23:34 -0500

    Update Figuras.java

--- src/modelo/Figuras.java
+++ src/modelo/Figuras.java
@@ -1,3 +1,14 @@
+/*
+ * ------------------------------------------------------------
+ * Name Proyecto: miniProyecto-1
+ * nombre participantes: Alejandro , Marvin Nuñez
+ * File: Figuras.java
+ * Relaciones: subclase en donde se organizan y se obtienen las ubicaciones de las imagenes (figuras)
+ * Ultima modificacion 06-10-23
+ * Pogramación Orientada a Objetos Grupo 80
+ * ------------------------------------------------------------
+ */
+
 package modelo;
 
 import java.lang.Math;
@@ -7,13 +18,16 @@ import java.util.List;
 
 
 public class Figuras{
+  //Atributos
   private String figuras[]= {"circulo","cuadrado","triangulo","doblecirculo","luna"};
   private String color[]={"amarillo","rojo","verde"};
   private int lado[] = new int[4];
 
+  //Método Constructor
   public Figuras(){
   }
 
+  //Métodos Getters
   public String getUbicacion(){
     int numAleatorio = (int)(Math.random()*5);
     int numAleatorio2 = (int)(Math.random()*3);
@@ -25,7 +39,10 @@ public class Figuras{
     return this.lado[index];
   }
 
-
+  /*
+  Se guarda en una lista las dimensiones establecidas, luego con el método shuffle
+  de logra reordenar estos valores de manera aleatoria
+  */
   public void barajarDimension(){
     List<Integer> values = Arrays.asList(40,60,80);
     Collections.shuffle(values);
